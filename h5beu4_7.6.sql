9818f2049d31a3e1ba852c9347d76494a3ac9176
KBence210 <79148226+KBence210@users.noreply.github.com>
2021-11-09 22:01:07 +0100

    Add files via upload

---
+++ h5beu4_7.6.sql
@@ -0,0 +1,17 @@
+create database wh8l7e_6;
+
+use wh8l7e_6;
+
+CREATE TABLE termekek (
+tkod INT PRIMARY KEY,  
+nev VARCHAR(50) NOT NULL,
+ear INT CHECK (EAR > 0), 
+kategoria CHAR(20)
+); 
+
+Insert into termekek values(1, 'citrom', 15, 'C'); /*rekord felvitele */
+Delete from termekek where Ear<X; /* Az X-nél olcsóbb termékek törlése*/
+Update termekek set ear= ear*0.9 where kategoria=Y; /*az Y kategóriájú termékek árának csökkentése 10%-al */
+
+
+
