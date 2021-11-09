520df7e7160fcdc53590a3ed0a06581e1ab4bce5
KBence210 <79148226+KBence210@users.noreply.github.com>
2021-11-09 21:59:59 +0100

    Add files via upload

---
+++ H5BEU4_1020/h5beu4_6.1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE wh8l7e_1;
+
+USE wh8l7e_1;
+
+CREATE TABLE gyarto (
+adoszam INT PRIMARY KEY,
+nev VARCHAR(30) NOT NULL,
+telephely varchar(100) );
+ 
+CREATE TABLE termek (
+tkod INT PRIMARY KEY,
+nev VARCHAR(30) NOT NULL,
+ear INT CHECK (EAR>0),
+gyarto INT REFERENCES GYARTO );
---
+++ H5BEU4_1020/h5beu4_6.2.sql
@@ -0,0 +1,29 @@
+create database wh8l7e_2;
+
+use wh8l7e_2;
+
+Create table gyarto(
+adoszam int primary key,
+nev varchar(30) NOT null,
+telephely varchar(50),
+irsz varchar(4),
+varos varchar(40),
+utca varchar(100) );
+
+Create table termek (
+tkod int primary key,  
+nev varchar(50) not null, 
+ear int check (EAR > 0), 
+gyarto int references GYARTO); 
+
+Create table alaktresz(
+akod  int primary key,  
+nev  varchar(50) not null);
+ 
+Create table egysegek(
+aru int references termek, 
+db int check (db > 0) ); 
+
+Create table komponens(
+termek int references termek, 
+alkatresz int references alkatresz);
