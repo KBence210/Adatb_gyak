d9f01d308d9a956a0dc832174471956f6d39cc52
KBence210 <79148226+KBence210@users.noreply.github.com>
2021-11-30 18:48:32 +0100

    Add files via upload

---
+++ h5beu4_1117/h5beu4_9_1.sql
@@ -0,0 +1,62 @@
+create database dchcas_8;
+
+use dchcas_8;
+
+create table gyarto(
+	adoszam INT PRIMARY KEY,
+	nev VARCHAR(30),
+    telephely VARCHAR(30),
+    irsz VARCHAR(4),
+	varos VARCHAR(40),
+    utca VARCHAR(100)
+);
+
+create table termek(
+	tkod INT(20) PRIMARY KEY,
+	nev VARCHAR(50),
+	ear INT CHECK(ear >0),
+    gyarto INT(3) REFERENCES gyarto(adoszam)
+);
+
+create table komponens(
+	termek INT(20) REFERENCES termek(tkod),
+    alkatresz INT(20) REFERENCES alkatresz(akod)	
+);
+
+create table alkatresz(
+	akod INT(30),
+    nev VARCHAR(50),
+    uj_ar INT(30)
+);
+
+create table egysegek(
+	aru INT(30) REFERENCES termek(tkod),
+    db INT CHECK(db>0)
+);
+insert into gyarto values(101,'A1','Miskolc',3543,'Miskolc','Damjanics');
+insert into gyarto values(102,'A2','Budapest',1103,'Budapest','Petofi');
+insert into gyarto values(103,'A3','Miskolc',3543,'Debrecen','Bartok');
+
+insert into termek values(1,'aso',4000,'A1');
+insert into termek values(2,'kapa',6000,'A1');
+insert into termek values(3,'gereblye',2000,'A1');
+insert into termek values(4,'lapat',1000,'A1');
+insert into termek values(5,'vodor',3000,'A2');
+
+insert into egysegek values(1,40);
+insert into egysegek values(2,10);
+insert into egysegek values(3,70);
+insert into egysegek values(4,100);
+insert into egysegek values(5,30);
+
+insert into komponens values('aso',201);
+insert into komponens values('kapa',202);
+insert into komponens values('gereblye',203);
+insert into komponens values('lapat',204);
+insert into komponens values('vodor',205);
+
+insert into alkatresz values(201,'nyel',3000);
+insert into alkatresz values(202,'fej',1000);
+insert into alkatresz values(203,'elezo',2000);
+
+select gyarto from termek where 
--- h5beu4_1117/h5beu4_9_2.sql
+++ h5beu4_1117/h5beu4_9_2.sql
@@ -1,1 +1,22 @@
+create database dchcas_9_2;
 
+use dchcas_9_2;
+
+create table tanfolyam(
+	tkod INT,
+    ar INT CHECK(ar>0),
+    megnevezes VARCHAR(50),
+    tipus VARCHAR(1)
+);
+
+create table befizetes(
+	kurzus INT REFERENCES tanfolyam(tkod),
+	diak boolean references resztvevo(tajszam),
+    befizetes  INT 
+);
+
+create table resztvevo(
+	tajszam varchar(12),
+    nev varchar(50),
+    lakcim varchar(100)
+);
